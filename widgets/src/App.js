ed1202bcf426b4e5c70a3665e9aac313460dd4ac
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-11-13 18:41:40 -0500

    second push

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
-
+import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -18,10 +19,27 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Red Color",
+    value: "red",
+  },
+  {
+    label: "The Green Color",
+    value: "green",
+  },
+  {
+    label: "The Blue Color",
+    value: "blue",
+  },
+];
+
 export default () => {
+ 
+
   return (
     <div>
-      <Search />
+      <Translate />
     </div>
   );
 };
