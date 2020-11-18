2146de237ea9f7d188db638ddeb58e4ce3fbd7ac
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-11-18 12:56:17 -0500

    footer-done

---
+++ portfolioweb/src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
+
+import Footer from './components/Footer';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Enrrique Pimentel",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" },
+      ],
+      home: {
+        title: "Be Amazing",
+        subTitle: "Projects that make the different",
+        text: "check out my projects below",
+      },
+      about: {
+        title: "About me",
+      },
+      contact: {
+        title: "Lets talk",
+      },
+    };
+  }
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Enrrique Pimentel</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Footer />
+        </Container>
+      </Router>
+    );
+  }
+}
+
+export default App;
