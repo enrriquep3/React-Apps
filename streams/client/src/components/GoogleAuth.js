aee2c424e70648282d8515d2e815fc72806c52ae
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-12-08 08:20:14 -0500

    googleAuth3

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:Auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "795101043614-46e3j6v5hacohfhk87j1abja09819d02.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "795101043614-46e3j6v5hacohfhk87j1abja09819d02.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
