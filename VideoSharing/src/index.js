2e215c45e57bf5cb10a0ef777d1d981c81600719
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-12-10 12:40:17 -0500

    VideoList

--- VideoSharing/src/index.js
+++ VideoSharing/src/index.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/searchBar";
+import VideoList from "./components/videoList";
+import VideoDetail from "./components/videoDetail";
 
 const API_KEY = "AIzaSyBq819BlwloHyW1vUElcy84fH0xwmH55t0";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    YTSearch({ key: API_KEY, term: "videos" }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
