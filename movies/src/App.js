14291ef5f13bd5d2d95588dbe37f9530675b2621
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-12-16 08:07:10 -0500

    header done

---
+++ movies/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <table style={{
+        background: '#222780',
+        display: 'block',
+        color: '#fff',
+        fontSize: 24,
+
+      }}>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://img.icons8.com/cotton/64/000000/film-reel.png"
+                alt="movie icon"
+              />
+            </td>
+            <td><h3>Movies and Shows Search</h3></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
