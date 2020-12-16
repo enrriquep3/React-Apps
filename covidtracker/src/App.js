11ba9101d0c95fd73b1996e3c3d05abbd22ff179
Enrrique Pimentel <65453362+enrriquep3@users.noreply.github.com>
2020-12-16 16:23:18 -0500

    CardsFetched

--- covidtracker/src/App.js
+++ covidtracker/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
 import { Cards, Charts, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends Component {
+   state =  {
+     data: {},
+   }
+
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+   
+
+this.setState({ data: fetchedData })
+  }
+
   render() {
+    const { data } = this.state;
     return (
-      <div>
-        <Cards />
+      <div className={styles.container}>
+        <Cards  data={data}/>
         <CountryPicker />
         <Charts />
       </div>
